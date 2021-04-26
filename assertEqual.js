995d4e144cc4516efc88c6d30f073f205d345bf9
felicia puspita yuwono <feliciaokta@outlook.com>
2021-04-26 16:42:19 -0700

    replace with literal syntax

--- assertEqual.js
+++ assertEqual.js
@@ -1,7 +1,7 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("Assertion Passed: " + actual + " === " + expected);
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
   } else {
     console.assert(actual === expected, (actual + " !== " + expected));
   }
