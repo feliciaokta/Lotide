057a3536eb7fb95d6bc6b403f5b102e1dbd27ecc
felicia puspita yuwono <feliciaokta@outlook.com>
2021-04-27 20:06:07 -0700

    added d2 assignment

---
+++ eqArrays.js
@@ -0,0 +1,28 @@
+// FUNCTION IMPLEMENTATION
+const eqArrays = function(actual, expected) {
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] === expected[i]) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+};
+
+const assertEqual = function(result, boolean) {
+  if (result === boolean) {
+    console.log("PASS");
+  } else {
+    console.log("FAIL");
+  }
+};
+
+
+// TEST CODE
+eqArrays([1, 2, 3], [1, 2, 3]) // => true
+eqArrays([1, 2, 3], [3, 2, 1]) // => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
