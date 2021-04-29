9397c9f8adab4e5ef7aec34978fe0ec8c75ae0f7
felicia puspita yuwono <feliciaokta@outlook.com>
2021-04-28 21:48:46 -0700

    added string literal

--- assertEqual.js
+++ assertEqual.js
@@ -3,7 +3,7 @@ const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`Assertion passed: ${actual} === ${expected}`);
   } else {
-    console.assert(actual === expected, (actual + " !== " + expected));
+    console.assert(actual === expected, (`${actual} !== ${expected}`));
   }
 };
 
