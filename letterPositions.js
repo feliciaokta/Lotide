4ee0489e218724f5923bdb0eca9215b9907eae8e
felicia puspita yuwono <feliciaokta@outlook.com>
2021-04-28 21:32:35 -0700

    output letter indices into an object

---
+++ letterPositions.js
@@ -0,0 +1,51 @@
+const eqArrays = function(actual, expected) {
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(result, boolean) {
+  if (result === boolean) {
+    console.log("PASS");
+  } else {
+    console.log("FAIL");
+  }
+};
+
+const countLetters = function(sentence) {
+  // return a count of how many times each letter appears
+  // do NOT count spaces
+  let answer = {};
+  for (const letter of sentence) {
+    if (letter !== ' ') {
+      if (answer[letter]) {
+        answer[letter] += 1;
+      } else {
+        answer[letter] = 1;
+      }
+    }
+  }
+  return answer;
+};
+
+
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    console.log(sentence[i], i);
+    if (sentence[i] !== ' ') {
+      // if (results[sentence[i]]) {
+      //   results[sentence[i]].push(i);
+      // } else {
+      //   results[sentence[i]] = [i]; // this square bracket means it's an array
+      // }
+      results[sentence[i]] ? results[sentence[i]].push(i) : results[sentence[i]] = [i]
+    }
+  }
+  return results;
+};
+
+console.log(letterPositions("This is exercise"));
