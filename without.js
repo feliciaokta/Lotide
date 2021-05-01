d2f5d8b5fd76c5494c0a4ab57d33a6e3f3b02e08
felicia puspita yuwono <feliciaokta@outlook.com>
2021-04-30 20:32:17 -0700

    first try assignment

--- without.js
+++ without.js
@@ -27,25 +27,27 @@ const assertEqual = function(result, boolean) {
 
 const without = function (source, remove) {
   let ansArray = [];
-  eqArrays(source, remove);
-  let index = source.indexOf(remove);
-  if (eqArrays === true) {
-    while (index >= 0) {
-      source.splice(index, 1);
-      index = source.indexOf(remove);
+  for (let i = 0; i < source.length; i++) {
+    if (remove[i] === source[i]) {
+      index = source.indexOf(remove[i]);
+      collected = source.splice(index, 1);
+      ansArray.push(source);
     }
-  } else if (eqArrays === false) {
-    ansArray.push(source);
   }
   return ansArray;
-}
+};
+// but this modifies the source array, which is not allowed
 
 // TEST CODE
-eqArrays([1, 2, 3], [1, 2, 3]) // => true
-eqArrays([1, 2, 3], [3, 2, 1]) // => false
+// eqArrays([1, 2, 3], [1, 2, 3]) // => true
+// eqArrays([1, 2, 3], [3, 2, 1]) // => false
 
-eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+// eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+// eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
 
+console.log(without([1, 2, 3], [1])) // => [2, 3]
+console.log(without(["1", "2", "3"], [1, 2, "3"])) // => ["1", "2"]
+const words = ["hello", "world", "lighthouse"];
+console.log(without(words, ["lighthouse"]));
