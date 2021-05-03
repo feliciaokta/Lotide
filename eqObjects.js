08161b7deb35e1a0c4e1b13771db2a17d3f01d86
felicia puspita yuwono <feliciaokta@outlook.com>
2021-05-02 17:33:06 -0700

    eqObjects updated with isArray

--- eqObjects.js
+++ eqObjects.js
@@ -18,41 +18,33 @@ const eqArrays = function(actual, expected) {
 // };
 
 
-// want to make function that checks whether 2 objects are exactly the same
-// two objects are equal when:
-// 1. they have the same number of keys
-// 2. The value for each key in 1st object is the same as the value for that same key in 2nd object
-
-// For the const eqObjects function:
-  // use the findKeyByValue exercise:
-  // find the values first in the 1st object,
-  // see if the key of that value matches the key in the other object
-  // loop the whole thing.
-  // need to use eqArrays function...
-  // Array.isArray([1, 2]) => true. This function is to determine whether the content is an array
-
-// inspect each key-value pair in both objects
-// compare the values
-
 const eqObjects = function(object1, object2) {
   let answer = "";
   let keys1 = Object.keys(object1);
   let keys2 = Object.keys(object2);
   let obj1length = keys1.length;
   let obj2length = keys2.length;
-  let isObj1ValueArray = keys1.isArray;
-  let isObj2ValueArray = keys1.isArray;
+  // let isObj1ValueArray = Array.isArray(keys1);
+  // let isObj2ValueArray = Array.isArray(keys2);
   // loop "for value in every object"??
   if (obj1length === obj2length) {
-    if (isObj1ValueArray === true && isObj2ValueArray === true) {
-      answer = eqArrays(object1, object2);
-      // this isArray doesn't work
-    } else if (object1[keys1] === object2[keys2]) {
+    for (let key in object1) {
+      let val1 = object1[key];
+      let val2 = object2[key];
+    if (Array.isArray(val1) && Array.isArray(val2)) {
+      for (let i = 0; i < val2.length; i++) {
+        if (val1[i] === val2[i]) {
+          answer = true;
+        } else {
+          answer = false;
+        }
+      }
+      }
+    }} else if (val1 === val2) {
       answer = true;
-    } else if (object1[keys1] !== object2[keys2]) {
+    } else if (val1 !== val2) {
       answer = false;
-    }
-  } else {
+    } else {
       answer = false;
     }
     return answer;
@@ -60,18 +52,20 @@ const eqObjects = function(object1, object2) {
 
 
 // expected output:
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-console.log(eqObjects(ab, ba)); // => true
+// const ab = { a: "1", b: "2" };
+// const ba = { b: "2", a: "1" };
+// console.log(eqObjects(ab, ba)); // => true
 
-const abc = { a: "1", b: "2", c: "3" };
-console.log(eqObjects(ab, abc)); // => false ... but my eqObjects returns true
+// const abc = { a: "1", b: "2", c: "3" };
+// console.log(eqObjects(ab, abc)); // => false
 
 const cd = { c: "1", d: ["2", 3] };
 const dc = { d: ["2", 3], c: "1" };
-console.log(eqObjects(cd, dc)); // => true
+console.log("question 1 ", eqObjects(cd, dc)); // => true
 
 const cd2 = { c: "1", d: ["2", 3, 4] };
-console.log(eqObjects(cd, cd2)); // => false
+console.log("question 2 ", eqObjects(cd, cd2)); // => false
 
-//http://adripofjavascript.com/blog/drips/object-equality-in-javascript.html
+// const arr1 = ["2", 3];
+// const arr2 = ["2", 3, 4];
+// console.log(eqArrays(arr2, arr1));
